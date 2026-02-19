4a59386c0dfa60603ee389fa8be21bcebe4a99d2
jeevana1409 <gandejeevanasai@gmail.com>
2026-02-19 22:15:25 +0530

    Update lib.groovy

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -1,3 +1,31 @@
 def gitDownload(repo)
 {
-  git "http:
+  git "https://github.com/jeevana1409/${repo}.git"
+}
+def build()
+{
+  sh 'mvn package'
+}
+def dockerContext()
+{
+  sh '''
+      mkdir -p docker 
+      cp webapp/target/webapp.war docker/webapp.war
+      cat <<EOF > docker/Dockerfile
+      FROM tomcat:10
+      COPY webapp.war /usr/local/tomcat/webapps
+      EXPOSE 8080
+      EOF
+      ''' 
+}
+def dockerBuild(imageName)
+{ 
+  sh 'docker build -t ${imageName} docker' 
+}
+def dockerContainer(containerName, imageName)
+{
+  sh """
+     docker rm -f ${containerName} || true
+     docker run -d  -p 8081:8080 --name ${containerName} ${imageName} 
+     """
+}
