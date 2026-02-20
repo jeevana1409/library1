a848367e9e38e9c0bb61b59e1b604c0000f756bd
jeevana1409 <gandejeevanasai@gmail.com>
2026-02-20 22:52:21 +0530

    Update devProd.groovy

--- vars/devProd.groovy
+++ vars/devProd.groovy
@@ -2,7 +2,7 @@ def gitDownload()
 {
   stage('clone')
   {
-    git "https://github.com/jeevana1409/FormFillApp.git"
+    git branch: "main", url: "https://github.com/jeevana1409/FormFillApp.git" 
   }
 }
     
